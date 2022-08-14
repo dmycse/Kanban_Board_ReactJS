8eac281267b228157bb004b6a7e5892e2a4452ee
dmycse <82826511+dmycse@users.noreply.github.com>
2022-08-14 23:24:08 +0300

    Update TaskSelection.js

--- src/components/main/TaskSelection/TaskSelection.js
+++ src/components/main/TaskSelection/TaskSelection.js
@@ -3,11 +3,10 @@ import css from './TaskSelection.module.css';
 const TaskSelection = ({status, selectList, tasksList, setTasksList, setSelectionListVisible}) => {
 
 	const handleChange = (e) => {
-		let findTask = tasksList.find(task => task.title === e.target.value)
-		let updateTasksList = tasksList.filter(task => task.title !== e.target.value)
-		
-		setTasksList([...updateTasksList, {...findTask, status: status}])
-		setSelectionListVisible(false)
+		let findTask = tasksList.find(task => task.title === e.target.value);
+		let updateTasksList = tasksList.filter(task => task.title !== e.target.value);
+		setTasksList([...updateTasksList, {...findTask, status: status}]);
+		setSelectionListVisible(false);
 	}
 
 	const handleClick = () => {
@@ -15,7 +14,7 @@ const TaskSelection = ({status, selectList, tasksList, setTasksList, setSelectio
 	}
 
 	return (
-    <div className={css.selection_block}>
+    		<div className={css.selection_block}>
 			<select name='task_selection' onChange={handleChange} className={css.select_task}>
 				<option value='none' className={css.select_option}>Select a task</option>
 				{selectList(status).map(task => <option key={task.id} value={task.title} >{task.title}</option>)}
